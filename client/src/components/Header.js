784bf194bca27ca096a770f2019403228cb3a0f0
Michael Standen <screaminghawk@gmail.com>
2019-02-21 08:20:44 +1300

    Add link to header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,9 +5,16 @@ const StyledHeader = styled.header`
 	width: 100%;
 	background-color: ${({ theme }) => theme.header};
 	color: ${({ theme }) => theme.main};
+	a {
+		color: ${({ theme }) => theme.accent};
+		font-size: 0.8em;
+	}
 	> div {
 		padding: 2em;
-		flex: 1;
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
+		align-items: center;
 	}
 `;
 
@@ -16,7 +23,9 @@ class Header extends Component {
 		return (
 			<StyledHeader>
 				<div>
-					testing
+					<span></span>
+					<h2>Baby Standen</h2>
+					<a href="https://michael.standen.link">Michael</a>
 				</div>
 			</StyledHeader>
 		)
