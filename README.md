6e933e0501d4769145fe0262f6557d04045e153b
Michael Standen <screaminghawk@gmail.com>
2019-02-20 13:36:58 +1300

    Add server.js first pass

--- README.md
+++ README.md
@@ -1,12 +1,29 @@
 # Baby.Standen.link
 
-It's a boy!!
-
 This website is for crowd sourcing names and voting on them.
+
+## Development
+
+### Start
+
+```sh
+yarn start
+```
+
+### Test
+
+```sh
+yarn test
+```
+
+### Notes
+
+Use `yarn`.
+
+Use `heroku`.
 
 ## Credits
 
 [Michael Standen](https://michael.standen.link)
 
 This software is provided under the [MIT License](https://tldrlegal.com/license/mit-license) so it's free to use so long as you give me credit.
-
---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+const path = require('path')
+const log = require('winston')
+
+// Configure logger
+log.remove(log.transports.Console)
+log.add(log.transports.Console, {
+	timestamp: true,
+	level: 'debug',
+})
+
+const app = express()
+
+const clientFolder = path.join(__dirname, 'client/build')
+
+let names = {}
+
+// Serve static files
+app.use(express.static(clientFolder))
+
+// Returns names
+app.get('/names', (req, res)=>{
+	res.json(names)
+})
+
+// Fail over
+app.put('*', (req, res)=>{
+	res.sendFile(path.join(clientFolder, 'index.html'))
+})
+
+// Start up
+const port = process.env.PORT || 5000
+app.listen(port)
+
+log.info(`Listing on port ${port}`)
